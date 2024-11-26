c559177dcca34e683c26ca8d5724624266b07239
WiSmKo <william@thewhisk.dev>
2024-11-26 20:44:31 +0100

    Add genres

--- app/search/components/AlbumTile.tsx
+++ app/search/components/AlbumTile.tsx
@@ -9,14 +9,23 @@ interface AlbumTileProps {
 
 export default function AlbumTile(props: AlbumTileProps) {
     return (
-        <div className="w-full h-full rounded-3xl overflow-hidden">
+        <div className="w-full h-full overflow-hidden">
             { <Image src={props.findRecordResponse.image} alt={props.findRecordResponse.title} width={150} height={150}/> }
             <div className='ml-4'>
                 <p><strong>{props.findRecordResponse.title}</strong></p>
                 <p>Artist: {props.findRecordResponse.artists}</p>
                 <p>Year: {props.findRecordResponse.year}</p>
                 <p>Price range: {props.findRecordResponse.latestPriceSuggestion?.toFixed(2) ?? 'N/A'}-{props.findRecordResponse.originalPriceSuggestion?.toFixed(2) ?? 'N/A'}</p>
-                <p>{props.findRecordResponse.summary}</p> 
+                
+                <div className='pt-4'>
+                    <p>{props.findRecordResponse.summary}</p>
+                </div>
+                 
+                <div className="pt-4 flex flex-wrap justify-center gap-5">
+                    {props.findRecordResponse.genres.map((genre, index) => (
+                        <span className="badge badge-success gap-2">{genre}</span>
+                    ))}
+                </div>
             </div>
         </div>
     )
