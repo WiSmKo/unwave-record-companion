5900e89f1d58626b2c27052c1f3a1f0ea8e35cc0
WiSmKo <william@thewhisk.dev>
2025-02-24 07:50:51 +0100

    clean references

--- app/tos/page.tsx
+++ app/tos/page.tsx
@@ -12,14 +12,14 @@ import config from "@/config";
 // You are an excellent lawyer.
 
 // I need your help to write a simple Terms & Services for my website. Here is some context:
-// - Website: https://shipfa.st
-// - Name: ShipFast
-// - Contact information: marc@shipfa.st
+// - Website: https://unwave.net
+// - Name: Unwave
+// - Contact information: william@unwave.net
 // - Description: A JavaScript code boilerplate to help entrepreneurs launch their startups faster
 // - Ownership: when buying a package, users can download code to create apps. They own the code but they do not have the right to resell it. They can ask for a full refund within 7 day after the purchase.
 // - User data collected: name, email and payment information
 // - Non-personal data collection: web cookies
-// - Link to privacy-policy: https://shipfa.st/privacy-policy
+// - Link to privacy-policy: https://unwave.net/privacy-policy
 // - Governing Law: France
 // - Updates to the Terms: users will be updated by email
 
